d37d068cc41d7b936dcd9f791a02250ed400b240
Oguzhan Ince <oguzhan824@gmail.com>
2022-09-25 01:36:35 +0300

    add: create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,29 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:tweet_errors] = @tweet.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   def destroy
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
