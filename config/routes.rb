6a6490895ea69f4802e1b39cad8bf152d67866a3
Oguzhan Ince <oguzhan824@gmail.com>
2022-09-25 01:03:19 +0300

    add: controll and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
-
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   devise_for :users
-  root to: 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tweets, except: [:edit, :update]
+  root to: 'tweets#index'
 end
