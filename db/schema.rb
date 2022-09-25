ff2b1ec3466508135fd54cd38d9ba34524e1797b
Oguzhan Ince <oguzhan824@gmail.com>
2022-09-25 03:29:26 +0300

    add: retweet migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_223740) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_25_002829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_223740) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "tweet_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
